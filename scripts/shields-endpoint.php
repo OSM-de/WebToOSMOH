07ca851c88da807947c5ec614e71dc813d2c9159
Kai Michael Poppe <kmpoppe@users.noreply.github.com>
2020-07-02 20:35:46 +0200

    Revert "Create shields-endpoint.php"
    This reverts commit 83c80f56a18463a44c702fda55e12bed255a1309.

--- scripts/shields-endpoint.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $j = json_decode(file_get_contents("https://api.countapi.xyz/get/osm-de-telegram/webtoosmoh-usage"));
-?>
-{
-  "schemaVersion": 1,
-  "label": "No. of conversions",
-  "message": "<?php echo $j->value; ?>",
-  "color": "#3866af"
-}
